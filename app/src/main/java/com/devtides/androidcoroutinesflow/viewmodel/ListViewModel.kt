5b8ce15beeaebc19fdb37c648b8d2febc04c849f
uma <uma2011ece@gmail.com>
2022-01-06 11:02:26 -0500

    Added NewsRepository

--- app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/devtides/androidcoroutinesflow/viewmodel/ListViewModel.kt
@@ -2,9 +2,14 @@ package com.devtides.androidcoroutinesflow.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.devtides.androidcoroutinesflow.model.NewsRepository
 
 class ListViewModel: ViewModel() {
 
-    val newsArticles = MutableLiveData<String>()
+   // val newsArticles = MutableLiveData<String>()
+
+    //here we are converting flow into LiveData
+    val newsArticle=NewsRepository().getNewsArticle().asLiveData()
 
 }
